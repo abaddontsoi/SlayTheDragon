5cef789cebaa47a859f8c5c71395f734e8f4bd86
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-11-17 08:34:00 +0800

    Created testing package

---
+++ src/unitTests/TestEntityStatus.java
@@ -0,0 +1,19 @@
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestEntityStatus {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
