e8021bfa5ba9a308495ad8bbe2f365a6caaaf036
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2024-11-19 01:15:41 +0800

    WIP on dev_abaddon_record_v2

---
+++ src/unitTests/record/turnDataType/TestTurnDataType.java
@@ -0,0 +1,22 @@
+package unitTests.record.turnDataType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import record.turnDataType.TurnDataType;
+
+class TestTurnDataType {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test_1() {
+		TurnDataType t = TurnDataType.AttackType;
+		assertEquals("Attack", t.toString());
+	}
+
+}
